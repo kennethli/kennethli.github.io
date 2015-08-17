ad30377972a3fac5f01af6cec07cfe67ff1dfa15
kennethli <kenneth@kennethli.me>
2015-08-16 23:26:08 -0400

    Header changes

--- header.php
+++ header.php
@@ -10,50 +10,110 @@
  */
 
 ?>
-<!doctype html>
-<html class="no-js" <?php language_attributes(); ?> >
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-		<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/favicon.ico" type="image/x-icon">
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/apple-touch-icon-144x144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/apple-touch-icon-114x114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/apple-touch-icon-72x72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/apple-touch-icon-precomposed.png">
-		
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/favicon.ico" type="image/x-icon">
+
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/app.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.5.8/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.5.8/slick-theme.css"/>
+    <script src="js/vendor/modernizr.js"></script>
+	<script src="/js/vendor/jquery.js"></script>
+	<script src="/js/vendor/fastclick.js"></script>
+	<script src="/js/foundation.min.js"></script>
+    <script src="//use.typekit.net/zwi2ydl.js"></script>
+    <script>
+try{Typekit.load();}catch(e){}
+    </script>
 		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
-	<?php do_action( 'foundationpress_after_body' ); ?>
-	
-	<?php if ( get_theme_mod( 'wpt_mobile_menu' ) == 'offcanvas' ) : ?>
-	<div class="off-canvas-wrap" data-offcanvas>
-	<div class="inner-wrap">
-	<?php endif; ?>
-	
-	<?php do_action( 'foundationpress_layout_start' ); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+    <header class="row">
+	    <div class="small-12 columns jgh-header hide-for-small-only">
+	        <div class="small-12 large-12 columns jgh-header-child">
+	            <h1 class="jgh-title small">&nbsp;The&nbsp;</h1>
+
+	            <h1 class="jgh-title big">Journal</h1>
+
+	            <h1 class="jgh-title small">&nbsp;of&nbsp;</h1>
+
+	            <h1 class="jgh-title big">Global Health</h1>
+	        </div>
+	    </div>
+	   <nav class="top-bar small-12 columns jgh-top-bar" data-topbar role="navigation">
+
+	     <ul class="title-area">
+
+	       <li class="name">
+
+	         <h1>JGH</h1>
+
+	       </li>
+  		 <li class="has-form">
+  		         <div class="row collapse">
+  		           <div class="large-8 small-9 columns">
+  		             <input type="text" placeholder="Find Stuff">
+  		           </div>
+  		           <div class="large-4 small-3 columns">
+  		             <a href="#" class="alert button expand">Search</a>
+  		           </div>
+  		         </div>
+  		       </li>
+
+	        <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+
+	       <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+
+	     </ul>
 
 
-	<?php if ( get_theme_mod( 'wpt_mobile_menu' ) == 'offcanvas' ) : ?>
-	<?php $GLOBALS['offcanvasposition'] = 'right';  // Can be 'left' or 'right'?>
-	<nav class="tab-bar">
-		<section class="<?php echo $GLOBALS['offcanvasposition'];?>-small">
-			<a class="<?php echo $GLOBALS['offcanvasposition'];?>-off-canvas-toggle menu-icon" href="#"><span></span></a>
-		</section>
-		<section class="middle tab-bar-section">
-			
-			<h1 class="title">
-				<?php bloginfo( 'name' ); ?>
-			</h1>
 
-		</section>
-	</nav>
+	     <section class="top-bar-section">
 
-	<?php get_template_part( 'parts/off-canvas-menu' ); ?>
-	<?php endif; ?>
+	       <!-- Right Nav Section -->
 
-	<?php get_template_part( 'parts/top-bar' ); ?>
+	       <ul class="right">
 
-<section class="container" role="document">
-	<?php do_action( 'foundationpress_after_header' ); ?>
+
+		        <li class="active"><a href="#">Right Button Active</a></li>
+		        <li class="has-dropdown">
+		          <a href="#">Right Button Dropdown</a>
+		          <ul class="dropdown">
+		            <li><a href="#">First link in dropdown</a></li>
+		            <li class="active"><a href="#">Active link in dropdown</a></li>
+		          </ul>
+		        </li>
+	       </ul>
+
+
+
+	       <!-- Left Nav Section -->
+
+		  <ul class="left">
+		       
+		      </ul>
+
+	     </section>
+
+	   </nav>
+
+
+	    <div class="small-12 columns jgh-header show-for-small-only">
+	        <div class="small-12 large-12 columns jgh-header-child">
+	            <h1 class="jgh-title small">&nbsp;The&nbsp;</h1>
+
+	            <h1 class="jgh-title big">Journal</h1>
+
+	            <h1 class="jgh-title small">&nbsp;of&nbsp;</h1>
+
+	            <h1 class="jgh-title big">Global Health</h1>
+	        </div>
+	    </div>
+    </header>
+
