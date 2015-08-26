4c93563cdff2f81f8b708dc4709f501a8a40b73f
Kenneth Li <kenneth@kennethli.me>
2015-08-26 00:21:54 -0400

    js

--- index.php
+++ index.php
@@ -298,8 +298,6 @@ get_header(); ?>
             </div>
         </div>
     </footer>
-$(document).foundation();
-    </script>
 
   <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
   <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
